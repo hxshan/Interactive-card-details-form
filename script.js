71461fffc19311fc0a420a658e62a70e07a3fad0
hxshan <galgodageheshan@gmail.com>
2023-02-24 19:54:31 +0530

    mobile version complete

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 const input_fields = document.querySelectorAll(".input-field")
 const submit_btn=document.querySelector(".submit-btn")
+const cardregex= /^[0-9]{16}$/;
 
- 
+//card holder name variables
+let card_holder_name = document.getElementById("name")
+let card_holder_name_text = document.querySelector(".card-name")
+
+//card numbers variables
+let card_number = document.getElementById("card-number")
+let card_number_text = document.querySelector(".cardnumber-text")
+// card dates variables
+let exp_month = document.getElementById("exp-month-in")
+let exp_month_text = document.querySelector(".exp-month")
+
+let exp_year = document.getElementById("exp-year-in")
+let exp_year_text = document.querySelector(".exp-year")
+//cvc variables
+let cvc = document.getElementById("cvc-in")
+let cvc_text = document.querySelector(".cvc-text")
+
+
+//validate wether data is entered
 function validate () {
-    let card_holder_name = document.getElementById("name")
-    let car_number = document.getElementById("card-number")
-    let exp_month = document.getElementById("exp-month-in")
-    let exp_year = document.getElementById("exp-year-in")
-    let cvv = document.getElementById("cvv-in")
-   
+//card holder name check
     if(card_holder_name.value.trim()===''){
         let error=input_fields[0].getElementsByClassName("error")
         if(error[0].classList.contains("hidden")){
             error[0].classList.remove("hidden")
         }
-        else{
-            error[0].classList.add("hidden")
-        }
+       
     }else{
         let error=input_fields[0].getElementsByClassName("error")
         if(!error[0].classList.contains("hidden")){
@@ -24,18 +36,23 @@ function validate () {
         }
       
     }
-
-   /* if(car_number.value.trim()===''){
+//number entered check
+   if(card_number.value.trim()===''){
         let error=input_fields[1].getElementsByClassName("error")
+        error[0].innerText="Can't be blank"
         if(error[0].classList.contains("hidden")){
             error[0].classList.remove("hidden")
         }
-        else{
-            error[0].classList.add("hidden")
-        }
-    }
-    else if(){
+       
         
+    }
+    else if(!cardregex.test(card_number.value.trim())){
+        let error=input_fields[1].getElementsByClassName("error")
+        error[0].innerText="Wrong format,numbers only"
+        if(error[0].classList.contains("hidden")){
+            error[0].classList.remove("hidden")
+        }
+
     }
     else{
         let error=input_fields[1].getElementsByClassName("error")
@@ -43,10 +60,92 @@ function validate () {
             error[0].classList.add("hidden")
         }
       
-    }*/
+    }
+ //Expiry date enterd check
+
+    if((exp_year.value.trim() === "") || (exp_month.value.trim() === '' )){
+
+       let dates_err_con=document.querySelector(".dates")
+       let date_err=dates_err_con.querySelector("#date-err")
+
+        if(date_err.classList.contains("hidden")){
+            date_err.classList.remove("hidden")
+      }
+   
+    
+    }
+
+    else
+    {
+
+        let dates_err_con=document.querySelector(".dates")
+        let date_err=dates_err_con.querySelector("#date-err")
+        if(!date_err.classList.contains("hidden")){
+            date_err.classList.add("hidden")
+        }    
+    }
+    if((cvc.value.trim() === "")){
+
+        let dates_err_con=document.querySelector(".dates")
+        let cvc_err=dates_err_con.querySelector("#cvc-err")
+ 
+         if(cvc_err.classList.contains("hidden")){
+            cvc_err.classList.remove("hidden")
+       }
+    
+     
+     }
+ 
+     else{
+         let dates_err_con=document.querySelector(".dates")
+         let cvc_err=dates_err_con.querySelector("#cvc-err")
+         if(!cvc_err.classList.contains("hidden")){
+            cvc_err.classList.add("hidden")
+         }
   
     
-}
+    }
+}   
+
+card_holder_name.addEventListener('keyup',(e)=>{
+    card_holder_name_text.innerText=card_holder_name.value
+})
+
+card_number.addEventListener('keyup',(e)=>{
+    let number_string = card_number.value.toString().replace(/(.{4})/g,"$1 ")
+    card_number_text.innerHTML=number_string
+})
+
+
+exp_month.addEventListener('keyup',(e)=>{   
+    let exp_month_text_temp = exp_month.value
+    if(parseInt(exp_month_text_temp) <=9 ) {       
+        exp_month_text_temp="0" + exp_month.value.toString()
+        exp_month_text.innerText = exp_month_text_temp
+    }
+    else{
+        exp_month_text_temp = exp_month.value
+        exp_month_text.innerText = exp_month_text_temp
+    }
+    
+})
+
+exp_year.addEventListener('keyup',(e)=>{   
+    let exp_year_text_temp = exp_year.value
+    if(parseInt(exp_year_text_temp) <=9 ) {       
+        exp_year_text_temp="0" + exp_year.value.toString()
+        exp_year_text.innerText = exp_year_text_temp
+    }
+    else{
+        exp_year_text_temp = exp_year.value
+        exp_year_text.innerText = exp_year_text_temp
+    }
+    
+})
+
+cvc.addEventListener('keyup',(e)=>{
+    cvc_text.innerText=cvc.value
+})
 
 
 submit_btn.addEventListener('click' ,(e)=>{
